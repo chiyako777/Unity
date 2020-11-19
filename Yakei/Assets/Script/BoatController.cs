0f2c06be53f0c4fcd2ea7bc990c869b3032cca15
chiyako777 <chi2324ya8388ko108500@gmail.com>
2020-11-19 22:43:11 +0900

    灯台追加

--- Yakei/Assets/Script/BoatController.cs
+++ Yakei/Assets/Script/BoatController.cs
@@ -2,36 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//CharactorControllerコンポーネントの自動アタッチ設定
-[RequireComponent(typeof(CharacterController))]
+//Rigidbodyコンポーネントの自動アタッチ設定
+[RequireComponent(typeof(Rigidbody))]
 public class BoatController : MonoBehaviour
 {
 
-    private CharacterController _characterController;   //CharacterControllerのキャッシュ
+    private Rigidbody _rigidbody;   //Rigidbodyのキャッシュ
     private Transform _transform;   //Transformのキャッシュ
-    [SerializeField] private float moveSpeed = 1;   //移動速度(Inspectorから設定)
-    private Vector3 _moveVelocity;  //移動速度情報
+    
+    private float moveSpeed = 1;   //移動速度
+    private float moveStopSpeed = 0.005f;   //移動停止速度
+    private int moveStopCount = 0;  //移動停止用カウンタ
+
+    private float rotateSpeed = 0.3f;   //回転速度
+    private float rotateStopSpeed = 0.005f;   //回転停止速度
+    private int rotateStopCount = 0;  //回転停止用カウンタ
+
 
     void Start()
     {
         //各コンポーネントのキャッシュの取得（毎フレーム使用するので、負荷を下げるためにキャッシュする）
-       _characterController = GetComponent<CharacterController>(); 
+       _rigidbody = GetComponent<Rigidbody>(); 
        _transform = transform;
     }
 
     void Update()
     {
 
-        //縦横方向の入力を取得(-1 ~ 1) * moveSpeed
-        _moveVelocity.x = Input.GetAxis("Horizontal") * moveSpeed;
-        _moveVelocity.z = Input.GetAxis("Vertical") * moveSpeed;
+    }
 
-        //Debug.Log("x = " + _moveVelocity.x + " z = " + _moveVelocity.z);
-        //ボートの向きを変える(将来的にはもっと滑らかに向きが変わるようにしたい)
-        transform.LookAt(_transform.position + new Vector3(_moveVelocity.x,0,_moveVelocity.z));
+    void FixedUpdate()
+    {
+        //キー入力を取得(-1 ~ 1)
+        float x  = Input.GetAxis("Horizontal") * moveSpeed;
+        float z =  Input.GetAxis("Vertical");
 
-        //ボートを動かす
-        _characterController.Move(_moveVelocity * Time.deltaTime);
+        if(x != 0){
+            //yを軸として左右に回転
+            _rigidbody.AddTorque(new Vector3(0,x * rotateSpeed,0),ForceMode.Force);
+            rotateStopCount = 0;
+        }else{
+            //回転を停止
+            if(rotateStopCount <= 1/rotateStopSpeed){
+                //Debug.Log("回転停止中");
+                rotateStopCount++;
+                _rigidbody.angularVelocity = Vector3.Lerp(_rigidbody.angularVelocity,Vector3.zero,rotateStopCount*rotateStopSpeed);
+            }
+        }
+
+        //移動
+        Vector3 moveVelocity = Vector3.zero;
+        if(z > 0 && z != 0){
+            moveVelocity = _transform.forward;
+            _rigidbody.velocity = moveVelocity;
+            moveStopCount = 0;
+        }else if(z < 0 && z != 0){
+            moveVelocity = _transform.forward*-1;
+            _rigidbody.velocity = moveVelocity;
+            moveStopCount = 0;
+        }else{
+            //移動停止
+            if(moveStopCount <= 1/moveStopSpeed){
+                //Debug.Log("移動停止中");
+                moveStopCount++;
+                _rigidbody.velocity = Vector3.Lerp(moveVelocity,Vector3.zero,moveStopCount*moveStopSpeed);
+            }
+        }
+
 
     }
 }
---
+++ Yakei/Assets/Script/SearchLightController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//灯台のサーチライトコントローラー
+public class SearchLightController : MonoBehaviour
+{
+    private Transform _transform;
+    private float rotateSpeed = 1.0f;
+    private float offsetY = 0.0f;
+
+    void Start()
+    {
+        _transform = transform;
+    }
+
+    void Update()
+    {
+        //Y軸を回転
+        offsetY += rotateSpeed;
+        //Debug.Log("offsetY = " + offsetY);
+        if(offsetY >= 360){
+            //Debug.Log("kiki");
+            offsetY = 0.0f;     
+            //y軸のtransform.rotationは基本的には-180~180の値を取っている
+            //（もし絶対値がそれ以上になっても自動で丸められる）
+            //しかし、そのままにしておくといつか桁あふれするので、360になったら0に戻すと、ちょうど切りがよく数値がリセットされる
+        }
+        transform.rotation = Quaternion.Euler(140,offsetY, 0);
+
+    }
+}
