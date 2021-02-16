d7d5a749b6fceeef71bbca7345f389a1922667a9
chiyako777 <chi2324ya8388ko108500@gmail.com>
2021-02-17 00:57:02 +0900

    20210217_0056

---
+++ NAGISA/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class CollisionDetector : MonoBehaviour
+{
+    [SerializeField] private TriggerEvent onTriggerStay = new TriggerEvent();
+
+    //is TriggerがONで、他のColliderと重なっているときは、このメソッドが常にコールされる
+    private void OnTriggerStay(CollisionDetector other){
+        onTriggerStay.Invoke(other);
+    }
+
+    // [Serializable]
+    // public class TriggerEvent : UnityEvent<CollisionDetector>{
+
+    // }
+
+}
