593ee3f76a38cac9b7377cf6dd289ad2f96f1e70
chiyako777 <chi2324ya8388ko108500@gmail.com>
2021-03-08 14:17:19 +0900

    フォルダ整理、シーン遷移ガワ処理

---
+++ NAGISA/Assets/Scripts/MapObject/MOTransition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MOTransition : MapObjectBase
+{
+    public string dist;
+    protected override IEnumerator OnAction(){
+
+        yield return null;
+
+        SceneManager.LoadScene(dist);
+
+        yield return new WaitForSeconds(1);
+
+        //※暗転等の演出は後で入れる
+
+    }
+}
