ef2c50f90cf7d0c8ff1cbd1980b6ab11e7fcc791
chiyako777 <chi2324ya8388ko108500@gmail.com>
2021-03-09 00:58:01 +0900

    20210309_0057

---
+++ NAGISA/Assets/Scripts/SpellPracController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class SpellPracController : MonoBehaviour
+{
+
+    private Text stageSelectText;
+    private List<RoomData> roomData;
+
+    void Start()
+    {
+        //** コンポーネント取得
+        Text[] text = GameObject.FindObjectsOfType<Text>();
+        foreach(Text t in text){
+            if(t.name == "stageSelect_text"){
+                stageSelectText = t;
+            }
+        }
+
+        //** ルームデータ読み込み
+        string f = Application.persistentDataPath + "/data/" + "room.json";
+        //roomData = JsonUtility.FromJson<List<RoomData>>(File.ReadAllText(f));
+        RoomData rData = JsonUtility.FromJson<RoomData>(File.ReadAllText(f));
+        Debug.Log("roonName = " + rData.name);
+        for(int i=0; i<rData.spell.Count; i++){
+            Debug.Log("spell = " + rData.spell[i]);
+        }
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ NAGISA/Assets/Scripts/Title/TitleMenu.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    private Text menuText;
+    private int selected;   //1start
+
+    void Start()
+    {
+        //** タイトルメニュー設定
+        selected = 1;
+        Text[] text = GameObject.FindObjectsOfType<Text>();
+        foreach(Text t in text){
+            if(t.name == "TitleMenu_text"){
+                menuText = t;
+            }
+        }
+        menuText.text = "  *ニューゲーム   コンティニュー   弾幕プラクティス   ゲーム終了";
+
+    }
+
+    void Update()
+    {
+        //** タイトルメニュー選択
+        if(Input.GetButtonDown("Horizontal") && Input.GetAxis("Horizontal")>0.0f){
+            menuText.text = menuText.text.Replace("*"," ");
+            selected = (int)Mathf.Clamp(selected + 1,0.0f,4.0f);
+            updateSelectedMark();
+        }
+        if(Input.GetButtonDown("Horizontal") && Input.GetAxis("Horizontal")<0.0f){
+            menuText.text = menuText.text.Replace("*"," ");
+            selected = (int)Mathf.Clamp(selected - 1,0.0f,4.0f);
+            updateSelectedMark();
+        }
+
+        //** 確定
+        if(Input.GetButtonDown("Submit")){
+            switch(selected){
+                case 1:     //ニューゲーム
+                    break;
+                case 2:     //コンティニュー
+                    break;
+                case 3:     //弾幕プラクティス
+                    toSpellPractice();
+                    break;
+                case 4:     //ゲーム終了
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    private void updateSelectedMark(){
+        switch(selected){
+            case 1:     //ニューゲーム
+                menuText.text = menuText.text.Replace("   ニューゲーム","  *ニューゲーム");
+                break;
+            case 2:     //コンティニュー
+                menuText.text = menuText.text.Replace("   コンティニュー","  *コンティニュー");
+                break;
+            case 3:     //弾幕プラクティス
+                menuText.text = menuText.text.Replace("   弾幕プラクティス","  *弾幕プラクティス");
+                break;
+            case 4:     //ゲーム終了
+                menuText.text = menuText.text.Replace("   ゲーム終了","  *ゲーム終了");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void toSpellPractice(){
+        SceneManager.LoadScene("SpellPracticeSetting");
+    }
+}
