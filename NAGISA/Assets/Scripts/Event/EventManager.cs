af9b9c4cca9ac158d5d7fba59539dbc48505c7ab
ys <chi2324ya8388ko108500@gmail.com>
2021-04-07 06:40:14 +0900

    test

---
+++ NAGISA/Assets/Scripts/Event/EventManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+
+    public class EventQueue{
+        public IEnumerator coroutine; 
+        public string eventType;
+        public int eventId;
+    }
+    private List<EventQueue> eventList = new List<EventQueue>();
+
+    void Start(){
+
+    }
+    void Update(){
+
+    }
+
+    // //** イベントキュー先頭のイベントを実行
+    private void Exec(){
+
+        if(!IsExec()){
+            return;
+        }
+
+        eventList[0].coroutine = CreateCoroutine();
+        StartCoroutine(eventList[0].coroutine);
+
+    }
+
+    //** 本当に実行していいかの何らかの判定が必要ならここで
+    private bool IsExec(){
+        return true;
+    }
+
+    private IEnumerator CreateCoroutine(){
+        switch(eventList[0].eventType){
+            case "Conv":
+                break;
+            case "Trans":
+                break;
+            case "":
+                break;
+        }
+        yield return null;
+    }
+
+}
