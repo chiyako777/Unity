04f9c4a5832bfa65a83bc1307e7747bb330e7c13
chiyako777 <chi2324ya8388ko108500@gmail.com>
2021-04-17 02:37:54 +0900

    EventProcessブランチ統合

--- NAGISA/Assets/Scripts/(NotUsed)MapObject/MapObjectBase.cs
+++ NAGISA/Assets/Scripts/(NotUsed)MapObject/MapObjectBase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//commit test
+//branch merge
 public abstract class MapObjectBase : MonoBehaviour
 {
     public Canvas window;
